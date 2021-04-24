d22b5c4a86e0bd87d259d5c55289bf89c98e53b9
itzzz exploit <82063930+sazgarpinky@users.noreply.github.com>
2021-04-24 18:08:31 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -15,23 +15,23 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setThumbnail(`https://media.discordapp.net/attachments/815252825012568085/815266512414703656/image0.png `)
-    .setTitle(`**Calli Help**`)
+    .setThumbnail(` `)
+    .setTitle(`**itzzz exploit Help**`)
     .setDescription(`
 
 **User Commands**
-\`c?invite\` - \`c?support\` - \`c?about\`
-\`c?ping\` - \`c?prefix\` - \`c?uptime\`
+\`invite\` - \`support\` - \`about\`
+\`ping\` - \`prefix\` - \`uptime\`
 
 **Music Commands**
-\`c?play\` - \`c?skip\` - \`c?skipto\`
-\`c?stop\` - \`c?volume\` - \`c?nowplaying\`
-\`c?shuffle\` - \`c?search\` - \`c?resume\`
-\`c?remove\` - \`c?queue\` - \`c?filter\`
-\`c?loop\` - \`c?lyrics\` - \`c?radio\`
+\`play\` - \`skip\` - \`skipto\`
+\`stop\` - \`volume\` - \`nowplaying\`
+\`shuffle\` - \`search\` - \`resume\`
+\`remove\` - \`queue\` - \`filter\`
+\`loop\` - \`lyrics\` - \`radio\`
 
 **Links**
-[Support](https://discord.gg/5aENC4fNVj) - [Invite](https://discord.com/api/oauth2/authorize?client_id=803965149941071875&permissions=8&scope=bot)
+[Support]() - [Invite]()
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
